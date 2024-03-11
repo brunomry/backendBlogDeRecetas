ead1963b35bf4f090d5d30374b1dfd07f4e87870
brunomry <brunomadozzo@gmail.com>
2024-03-11 02:16:58 -0300

    En index.js importe dependencia de express y midlewares, invoque midlewares, agregue logica para que tome puerto de variable de entorno de archivo .env creado o puerto elegido para pruebas de desarrollo

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config";
+
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+  console.log("estoy en el puerto "+app.get("port"));
+})
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
