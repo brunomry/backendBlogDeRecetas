1075fe5be90028515ccb8f35f60fcb320a829b52
brunomry <brunomadozzo@gmail.com>
2024-03-15 03:34:18 -0300

    Agregue funcion para obtener receta por id e invoque la misma mediante solicitud get en nueva ruta creada en controlador

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { crearReceta, listarRecetas } from "../controllers/recetas.controllers.js";
+import { crearReceta, listarRecetas, obtenerReceta } from "../controllers/recetas.controllers.js";
 
 const enrutador = new Router();
 
 enrutador.route("/recetas").get(listarRecetas).post(crearReceta);
+enrutador.route("/receta/:id").get(obtenerReceta);
 
 export default enrutador;
